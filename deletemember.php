3ede9c26a629135789e38abeef2ed020e3d7165c
hfycb3 <103494565+hfycb3@users.noreply.github.com>
2022-04-19 15:07:18 +0800

    Update deletemember.php

--- deletemember.php
+++ deletemember.php
@@ -2,8 +2,17 @@
 require_once "config.php";
 if(isset($_GET['delete'])){
     $id=$_GET['delete'];
+    $sql ="SELECT * FROM borrow WHERE member_id=$id";
+    $result= $link->query($sql);
+    if($result->num_rows>0){
+        $link->close();
+        $delerror=2;
+        header("location: memberlist.php?delete=$delerror");
+    }
+    else
+    {
     $link->query("DELETE FROM member WHERE member_id=$id") or die($link->error());
-
     header("location: memberlist.php");
+    }
 }
 ?>
