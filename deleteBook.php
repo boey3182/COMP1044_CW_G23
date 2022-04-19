bc829f66bb786789418ae2adcd4b286a5a0d032b
hfycb3 <103494565+hfycb3@users.noreply.github.com>
2022-04-19 15:09:16 +0800

    Update deleteBook.php

--- deleteBook.php
+++ deleteBook.php
@@ -3,9 +3,19 @@
 session_start();
 
 require_once "config.php";
+$delerror="";
 if(isset($_GET['delete'])){
     $id=$_GET['delete'];
-    $link->query("DELETE FROM book WHERE book_id=$id") or die($link->error());
-    header("location: booklist.php");
-}
+    $sql = "SELECT * FROM borrowdetails WHERE book_id = $id";
+    $result = $link->query($sql);
+    if($result->num_rows>0){
+        $link->close();
+        $delerror= 2;
+        header("location: booklist.php?delete=$delerror");
+    }else{
+        $link->query("DELETE FROM book WHERE book_id=$id") or die($link->error());
+        header("location: booklist.php");
+    }
+    }
+
 ?>
